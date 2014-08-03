2d516e551e5537667b1065f6b25182229711329f
sgtn <sgtn@sgtn.local>
2014-08-03 22:52:40 +0900

    口上や間の修正

--- nim.rb
+++ nim.rb
@@ -6,75 +6,76 @@ require 'pp'
 require './calc.rb'
 
 num = []
-puts "\n"
-puts "うちの名前はぽよぽよだぽよ!"
-sleep 1.5
-puts "三山崩しをするぽよ!"
-sleep 1.5
-puts "一度に1から3つの石をとっていって、最後の石をとれた人の勝ちぽよ!"
-sleep 2.5
-puts "\n"
-puts "\n"
-puts "\n"
-3.times do |i|
-  num[i] = (2..6).to_a.sample
-end
-puts "うちが山を作ってあげてるぽよ!"
-sleep 2
 
-puts "\n"
+puts "\nうちの名前はぽよぽよだぽよ!"; sleep 1.5
+puts "三山崩しをするぽよ!"; sleep 1.5
+puts "一度に1から3つの石をとっていって、最後の石をとれた人の勝ちぽよ!"; sleep 2.5; puts "\n\n\n"
 
 my_turn = [true, false].sample
-puts my_turn ? 'あなたのターンからどうぞぽよ！' : 'うちのターンからいくぽよ！'
-puts "\n"
-sleep 1
+puts my_turn ? "今回はあなたのターンからどうぞぽよ！\n" : "うちのターンからいくぽよ！\n"; sleep 1
+
+3.times { |i| num[i] = (4..8).to_a.sample }
+puts "うちが山を作ってあげてるぽよ!"; sleep 2; puts "\n"
 
 100.times do |i|
-  rocks = ['1: ', '2: ', '3: ']
+  mountains = ['1: ', '2: ', '3: ']
 
   3.times do |j|
     num[j].times do
-      rocks[j] += '*'
+      mountains[j] += '*'
     end
-    puts rocks[j]
+    puts mountains[j]
   end
+  puts "\n"; sleep 1
 
-puts "\n"
-sleep 1
   if num.sum > 0
-    @user = my_turn ? 'あなた' : 'ぽよぽよ'
-    puts "\n"
-    puts "#{i+1}ターン目 : #{@user}のターンですぽよ！"
+    @user = my_turn ? "あなた" : "ぽよぽよ"
+    puts "\n#{i+1}ターン目 : #{@user}のターンですぽよ！"
     if my_turn
-      # あなた
       puts 'どの山から石をとりますかぽよ'
-      mt = gets.chomp.to_i - 1
+      selected_mountain = gets.chomp.to_i - 1
       puts '石をいくつとりますかぽよ？'
-      rid = gets.chomp.to_i
-      num[mt] = num[mt] - rid
+      remove_stone = gets.chomp.to_i
+      num[selected_mountain] = num[selected_mountain] - remove_stone
       sleep 0.8
     else
-      puts '考え中だぽよ'
-      sleep 2
-      # コンピューター
+      case num.sum
+      when 1
+        puts 'いただくぽよ!!'
+      when 2
+        puts 'ふむふむぽよ〜!'
+      when 3
+        puts 'ふふふぽよ!'
+      when 4
+        puts 'おもしろいぽよ!'
+      when 5
+        puts 'ふーむ、ぽよ!'
+      when 6
+        puts '読めてきたぽよ!'
+      when 7
+        puts 'きたきたぽよ!'
+      when 8
+        puts 'むむむぽよ!'
+      else
+        puts '考えてるぽよ...!'
+      end; sleep 2
+
+      # 排他的論理和による必勝アルゴリズム
       mt_xor = num[0] ^ num[1] ^ num[2]
       l = 0
-      3.times do |k|
-        l = k if num[k] > num[k] ^ mt_xor
-      end
+      3.times { |k| l = k if num[k] > num[k] ^ mt_xor }
 
+      # 必勝パターンに当てはまらなければ最大の山から1だけ取って相手のミスを待つ
       if l != 0 && 1 <= mt_xor && mt_xor <= 3
         num[l] ^= mt_xor
       else
         num[Calc.calc_max(num)[:id]] -= 1
-      end
-      sleep 2.5
+      end; sleep 2.5
     end
     my_turn = !my_turn
   else
     break
   end
-  puts "\n"
 end
 
-puts "#{@user}の勝ちぽよ！"
+puts "\n#{@user}の勝ちぽよ！"
